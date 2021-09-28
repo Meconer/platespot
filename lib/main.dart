4104a090bc7976e840fce0829fbbfbc1c5c054c3
Mats Andersson <mats.asson@gmail.com>
2021-09-28 12:01:56 +0200

    Numbering actually starts at 1 now. Added drawer menu and removed settings icon button Added about menu item (Om) Replaced floatingactionbutton with textbutton

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _loadCounter() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-      _counter = (prefs.getInt('counter') ?? 0);
+      _counter = (prefs.getInt('counter') ?? 1);
     });
   }
 
@@ -48,10 +49,10 @@ class _MyHomePageState extends State<MyHomePage> {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
       _counter++;
-      if (_counter == 1000 ) {
+      if (_counter == 1000) {
         _counter = 0;
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(' FÄRDIG!!**!!**!!')));
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(' FÄRDIG!!**!!**!!')));
       }
       prefs.setInt('counter', _counter);
     });
@@ -60,17 +61,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+                child: Container(
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Text(
+                      'Meny',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Ställ in nästa nummer'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _setCounter();
+                },
+              ),
+              ListTile(
+                title: const Text('Om'),
+                onTap: () {
+                  _showVersion();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text(widget.title),
-        actions: [
-          // action button
-          IconButton(
-            icon: Icon(Icons. settings),
-            onPressed: () {
-              _setCounter();
-            },
-          ),
-        ],
       ),
       body: Center(
         child: Column(
@@ -78,30 +104,50 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               'Nummer att leta efter:',
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 30,
+              )
             ),
             GestureDetector(
               onLongPress: _setCounter,
-              child: Text(
-                getTextForCounter(_counter),
-                style: Theme.of(context).textTheme.headline4,
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Text(
+                  getTextForCounter(_counter),
+                  style: Theme.of(context).textTheme.headline1,
+                ),
               ),
+            ),
+            TextButton(
+              child: Text('Hittat'),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                elevation: 10,
+                shape: RoundedRectangleBorder( borderRadius: BorderRadius.all(Radius.circular(10)),),
+                 textStyle: TextStyle(
+                   fontSize: 50,
+                   fontWeight: FontWeight.w300,
+                 ),
+              ),
+              onPressed: _incrementCounter,
             ),
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: SizedBox(
-        width: 200,
-        child: FittedBox(
-            child: FloatingActionButton.extended(
-              onPressed: _incrementCounter,
-              tooltip: 'Hittat',
-              icon: Icon(Icons.add),
-              label: Text('Hittat'),
-
-            ),
-          ),
-        ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // floatingActionButton: SizedBox(
+      //   width: 200,
+      //   child: FittedBox(
+      //     child: FloatingActionButton.extended(
+      //       onPressed: _incrementCounter,
+      //       tooltip: 'Hittat',
+      //       icon: Icon(Icons.add),
+      //       label: Text('Hittat'),
+      //     ),
+      //   ),
+      //),
     );
   }
 
@@ -110,7 +156,7 @@ class _MyHomePageState extends State<MyHomePage> {
     prefs.setInt('counter', number);
   }
 
-  void _setCounter( ) {
+  void _setCounter() {
     final _controller = TextEditingController(
       text: '$_counter',
     );
@@ -135,7 +181,7 @@ class _MyHomePageState extends State<MyHomePage> {
         TextButton(
           child: Text('Ok'),
           onPressed: () {
-            final number = int.parse( _controller.text);
+            final number = int.parse(_controller.text);
             setState(() {
               _counter = number;
               storeCounter(number);
@@ -151,6 +197,24 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (context) {
         return alert;
       },
+    );
+  }
+
+  void _showVersion() async {
+    final scaffoldMsngr = ScaffoldMessenger.of(context);
+    final packageInfo = await PackageInfo.fromPlatform();
+    final buildNumber = packageInfo.buildNumber;
+    final buildVersion = packageInfo.version;
+    final versionText = packageInfo.appName +
+        " Ver: " +
+        buildVersion +
+        " Build: " +
+        buildNumber;
+    Navigator.of(context).pop();
+    scaffoldMsngr.showSnackBar(
+      SnackBar(
+        content: Text(versionText),
+      ),
     );
   }
 }
